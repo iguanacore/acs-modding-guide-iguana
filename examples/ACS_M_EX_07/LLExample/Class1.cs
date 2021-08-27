35ee15e194156a83da7ef8f8202b97eb707346e5
iguanacore <gert.jarv@gmail.com>
2021-08-27 19:19:07 +0300

    Added ACS_M_EX_07, on harmony patching
    Harmony patching example solution with Visual Studio 2019, contains both MLL and Lua Loading examples.

---
+++ examples/ACS_M_EX_07/LLExample/Class1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using System.IO;
+using XiaWorld;
+using HarmonyLib;
+
+
+namespace LLExample
+{
+    public class LLExample
+    {
+        public static void Patch()
+        {
+            try
+            {
+                Assembly.LoadFrom(Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "0Harmony.dll"));
+                Harmony harmony = new Harmony("LLExample.Mod");
+                harmony.PatchAll();
+                KLog.Dbg("[LLExample] Loaded!");
+            }
+            catch (Exception e)
+            {
+                KLog.Dbg("[LLExample] error" + e.ToString());
+            }
+        }
+
+        [HarmonyPatch(typeof(BodyPractice), "GetItemCost")]
+        class LLExamplePatch
+        {
+            static void Postfix(ref float __result, string part)
+            {
+                KLog.Dbg("[LLExample] GetItemCost (" + __result + ")");
+                __result = 1f;
+            }
+        }
+    }
+}
